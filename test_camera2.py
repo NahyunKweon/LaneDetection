b7f5183112b7c1b7b31a0afdd49b14f5c413b224
glen <skgus1045@hanyang.ac.kr>
2024-09-15 16:54:49 +0900

    capture by key

---
+++ test_camera2.py
@@ -0,0 +1,31 @@
+from sys import stdin
+from termios import TCIOFLUSH, tcflush
+from time import strftime
+
+from keyboard import is_pressed
+
+from picamera2 import Picamera2, Preview
+#libcamera-hello -t 0 --qt-preview
+key_flag = False
+cam = Picamera2()
+#cam.start_preview(Preview.QTGL)
+cam.start()
+
+try:
+    while True:
+        if is_pressed('p'):
+            if key_flag is False:
+                key_flag = True
+            filename = strftime("%Y%m%d-%H%M%S") + '.png'
+            cam.capture_file(filename, format="png", wait=None)
+            print(f"\rCaptured {filename} succesfully")
+        else:
+            key_flag = 0
+        if is_pressed('q'):
+            print("\rClosing camera...")
+            break
+finally:
+    #cam.stop_preview()
+    cam.stop()
+    cam.close()
+    tcflush(stdin, TCIOFLUSH)
